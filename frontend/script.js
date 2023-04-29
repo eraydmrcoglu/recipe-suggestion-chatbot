48e865e0b697c1058aad8b74dccd3b1658a90738
Kadir Burak Gündüz <kadir.burak.gunduz@gmail.com>
2023-04-29 09:40:21 +0300

    Fix image extensions

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
-import bot from './assets/bot.png';
-import user from './assets/user.png';
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
 
 const form = document.querySelector('form')
 const chatContainer = document.querySelector('#chat_container')

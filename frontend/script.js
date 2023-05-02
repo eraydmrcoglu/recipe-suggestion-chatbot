9607aa0e9009358e07c3c7b2554e82a257d6f001
Eray Demircioğlu <c.eray@hotmail.com>
2023-05-02 09:34:56 +0300

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     } else {
         const err = await response.text()
 
-        messageDiv.innerHTML = "Something went wrong"
+        messageDiv.innerHTML = "Something went wrong."
         alert(err)
     }
 }

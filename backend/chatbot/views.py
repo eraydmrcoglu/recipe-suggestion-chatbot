f08c482b56b37776ec9eca3d307640ae3638366a
Kadir Burak Gündüz <kadir.burak.gunduz@gmail.com>
2023-05-02 07:57:44 +0300

    Add pre-commit to backend

--- backend/chatbot/views.py
+++ backend/chatbot/views.py
@@ -37,7 +37,9 @@ class ChatbotView(APIView):
         chatbot_response = get_chatbot_response(chat_history=chat_history)
 
         if "OpenAI API Error" in chatbot_response:
-            return Response(chatbot_response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(
+                chatbot_response, status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
 
         CHATS[session_id].append({"role": "user", "content": prompt})
         CHATS[session_id].append({"role": "assistant", "content": chatbot_response})

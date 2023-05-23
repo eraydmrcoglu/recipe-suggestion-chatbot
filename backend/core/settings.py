3c2bdb3492729f99e766201c6e100bdc3d9242cb
Kadir Burak Gündüz <kadir.burak.gunduz@gmail.com>
2023-05-23 10:49:02 +0300

    Add localhost to ALLOWED_HOSTS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: list[str] = []
+ALLOWED_HOSTS: list[str] = ["127.0.0.1", "localhost"]
 
 
 # Application definition
